a30e0929e4cd635282a8cf25c10a0d7602e37f16
Kimdonghwan <kimdonghwan913@gmail.com>
2025-06-14 20:00:22 +0900

    refactor: store 모듈화

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,52 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import todoApp from './modules/todoApp';
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const array = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        array.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-      }
-    }
-    return array;
-  },
-};
-
 export const store = new Vuex.Store({
-  state: {
-    todoItems: storage.fetch(),
-  },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    },
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed =
-        !state.todoItems[payload.index].completed;
-
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    },
+  modules: {
+    todoApp,
   },
 });
